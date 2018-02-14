4a3d2ba25c57b9d99ab62e6a3a3f9b06f6752fb2
Stanbridge,Ashley N <s527921@ch140030m.nwmissouri.edu>
2018-02-14 17:03:55 -0600

    Added buttons and wrote comments as to what I wish to add in code for the IBActions

--- Specs/ViewController.swift
+++ Specs/ViewController.swift
@@ -19,7 +19,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBOutlet weak var userTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBAction func logIn(_ sender: Any) {
+        //For this action button, it should take the Username and Password on file and check to make sure the credentials are correct.
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+        //For this sign up button, when clicked, it will take the user to a new view so they can create an account
+    }
+    
+    
+    
 }
 
