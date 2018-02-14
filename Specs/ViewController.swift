fc2f196d33af32cc8ae5772719bc6cd7d42631f7
student <s523840@ch140032m.local>
2018-02-14 15:29:06 -0600

    Initial Commit

---
+++ Specs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Specs
+//
+//  Created by Reuscher,Elicia R on 2/14/18.
+//  Copyright © 2018 Reuscher,Elicia R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
